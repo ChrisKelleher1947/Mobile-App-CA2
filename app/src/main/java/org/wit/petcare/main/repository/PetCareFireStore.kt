9b940c4280dbe7ab553f4fb6867fff6a43dea479
ChrisKelleher1947 <ckelleher1312@gmail.com>
2025-12-16 01:02:07 +0000

    Updated firestore logic to include seperation of data by user

--- app/src/main/java/org/wit/petcare/main/repository/PetCareFireStore.kt
+++ app/src/main/java/org/wit/petcare/main/repository/PetCareFireStore.kt
@@ -1,37 +1,42 @@
 package org.wit.petcare.main.repository
 
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import org.wit.petcare.models.PetCareModel
 
 class PetCareFirestore : PetCareStore {
 
     private val db = FirebaseFirestore.getInstance()
-    private val petsCollection = db.collection("pets")
+    private val auth = FirebaseAuth.getInstance()
+
+    private fun petsCollection() =
+        db.collection("users")
+            .document(auth.currentUser!!.uid)
+            .collection("pets")
 
     override fun findAll(callback: (List<PetCareModel>) -> Unit) {
-        petsCollection.get()
+        petsCollection().get()
             .addOnSuccessListener { result ->
-                val pets = result.toObjects(PetCareModel::class.java)
-                callback(pets)
+                callback(result.toObjects(PetCareModel::class.java))
             }
     }
 
     override fun create(pet: PetCareModel, callback: () -> Unit) {
-        val doc = petsCollection.document()
+        val doc = petsCollection().document()
         pet.id = doc.id
-        doc.set(pet)
-            .addOnSuccessListener { callback() }
+        doc.set(pet).addOnSuccessListener { callback() }
     }
 
     override fun update(pet: PetCareModel, callback: () -> Unit) {
-        petsCollection.document(pet.id)
+        petsCollection().document(pet.id)
             .set(pet)
             .addOnSuccessListener { callback() }
     }
 
     override fun delete(petId: String, callback: () -> Unit) {
-        petsCollection.document(petId)
+        petsCollection().document(petId)
             .delete()
             .addOnSuccessListener { callback() }
     }
 }
+
